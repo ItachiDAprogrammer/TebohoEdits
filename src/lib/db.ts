49b79d69a39c381d5931d9834fc71525f0ecc04c
ItachiDAprogrammer <49144738+ItachiDAprogrammer@users.noreply.github.com>
2026-01-05 12:52:04 +0200

    temp fix to be reverted later

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from '@prisma/client'
 
+const DATABASE_URL = process.env.DATABASE_URL || 'file:./dev.db'
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL
+    }
+  },
+  log: ['query']
+})
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db
+}
